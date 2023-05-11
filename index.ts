e3801cc651fc0b7a6e96916107ff683c63e56a2f
ViktorLumiste <viktor.lumiste@gmail.com>
2023-05-11 13:30:41 +0300

    Faili sisu lugemine

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 // @ts-ignore
 import fs from 'fs';
-function algabKahega(element:string) {
-    return (Array.from(element)[0] == '1')
-}
 
-let filenames:string[] = fs.readdirSync('kolmapaev');
-let filecount:number = (fs.readdirSync('kolmapaev')).length;
-let fileone:string[] = (fs.readdirSync('kolmapaev')).filter(algabKahega);
-let fileonecount:number = fileone.length;
-console.log(filenames);
-console.log(filecount);
-console.log(fileonecount);
+let contents:string=fs.readFileSync("kolmapaev/123abc.txt", "utf-8");
+let contents1:string=fs.readFileSync("kolmapaev/223abc.txt", "utf-8");
+let contents2:string=fs.readFileSync("kolmapaev/323abc.txt", "utf-8");
+let contentsCount:number=Array.from(contents).length
+console.log(contents);
+console.log(contents+contents1);
+console.log(contents+contents1+contents2);
+console.log(contentsCount);
