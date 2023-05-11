a2dbe7553db5c4e94fba0a7333be4d1a45b55961
ViktorLumiste <viktor.lumiste@gmail.com>
2023-05-11 14:49:26 +0300

    Päevaste läbisõitude summa funktsioonina

--- index.ts
+++ index.ts
@@ -1,28 +1,45 @@
 // @ts-ignore
 import fs from 'fs';
-let startdir:string="teekond1/"
-let maxd = 0
-let maxn = ''
-function displayDirectoryData(dirname:string){
+function sumDirectoryData(dirname:string):number{
+    let sum: number=0;
+    let dirnames:string[] = fs.readdirSync(dirname);
 
-    console.log(dirname)
-    let filenames:string[] = fs.readdirSync(startdir+dirname);
-    for(let filename of filenames){
-        let contents:string=fs.readFileSync(startdir+dirname+"/"+filename, "utf-8");
-        if (parseInt(contents) <= 30){
-            contents = ''
+    for(let dirnam of dirnames){
+
+        let filenames:string[] = fs.readdirSync(dirname+'/'+dirnam);
+
+        for(let filename of filenames){
+            let contents:string=fs.readFileSync(dirname+"/"+dirnam+"/"+filename, "utf-8");
+            sum+=parseInt(contents);
         }
-        if (parseInt(contents)>maxd){
-            maxn = filename
-            maxd = parseInt(contents)
-        }
-        console.log("  "+filename, contents);
     }
-
+    return sum;
 }
+function maxDirectoryData(dirname:string){
+    let max: string ='';
+    let max1n: string='';
+    let max1d: number=0;
+    let max2n: string='';
+    let max2d: number=0;
+    let dirnames:string[] = fs.readdirSync(dirname);
 
-let dirnames:string[] = fs.readdirSync(startdir);
-for(let dirname of dirnames){
-    displayDirectoryData(dirname);
+    for(let dirnam of dirnames){
+
+        let filenames:string[] = fs.readdirSync(dirname+'/'+dirnam);
+
+        for(let filename of filenames){
+            let contents:string=fs.readFileSync(dirname+"/"+dirnam+"/"+filename, "utf-8");
+            if (dirnam == 'kolmapaev' && parseInt(contents)>max1d){
+                max1d = parseInt(contents)
+                max1n = filename
+            } else if(dirnam == 'neljapaev' && parseInt(contents)>max2d){
+                max2d = parseInt(contents)
+                max2n = filename
+            }
+        }
+    }
+    max = (`${max1n}-${max1d} \n${max2n}-${max2d}`  )
+    return max
 }
-console.log(maxn, maxd)
+console.log(sumDirectoryData("teekond1"));
+console.log(maxDirectoryData("teekond1"));
