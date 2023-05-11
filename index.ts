2e21a3235881f7d405f349ecf52c8c56293d2cc0
ViktorLumiste <viktor.lumiste@gmail.com>
2023-05-11 15:27:04 +0300

    Andmepuu kuvamine rekursiooniga

--- index.ts
+++ index.ts
@@ -1,33 +1,15 @@
 // @ts-ignore
 import fs from 'fs';
-function minDayData(dirname:string){
-    let values:number[]=fs.readdirSync(dirname).map(filename =>
-        parseInt(fs.readFileSync(dirname+"/"+filename, "utf-8") ));
-    return Math.min(...values);
-}
-function maxDayData(dirname:string){
-    let values:number[]=fs.readdirSync(dirname).map(filename =>
-        parseInt(fs.readFileSync(dirname+"/"+filename, "utf-8") ));
-    return Math.max(...values);
-}
-function diffDayData(dirname:string){
-    let diff:number= maxDayData(dirname) - minDayData(dirname)
-    return diff;
+
+function display(path:string):void{
+    if(fs.lstatSync(path).isDirectory()){
+        for(let subpath of fs.readdirSync(path)){
+            if (subpath == '123ABC.txt'){
+                console.log(path+"/"+subpath +' '+ fs.readFileSync(path+"/"+subpath));
+            }
+            display(path+"/"+subpath);
+        }
+    }
 }
 
-function minDaysData(startdir:string){
-    let daynames:string[]=fs.readdirSync(startdir);
-    let values:number[]=daynames.map(dayname => minDayData(startdir+dayname));
-    return Math.min(...values);
-}
-function maxDaysData(startdir:string){
-    let daynames:string[]=fs.readdirSync(startdir);
-    let values:number[]=daynames.map(dayname => maxDayData(startdir+dayname));
-    return Math.max(...values);
-}
-
-console.log(diffDayData("teekond1/neljapaev/"));
-console.log(minDayData("teekond1/neljapaev/"));
-console.log(maxDayData("teekond1/neljapaev/"));
-console.log(minDaysData("teekond1/"));
-console.log(maxDaysData("teekond1/"));
+display("./teekond2/neljapaev");
